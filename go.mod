a9adcd21bc522443b67f358137a4c5480a35de62
Anderson <anderson771218@gmail.com>
2019-12-20 21:52:21 +0800

    jwt auth server

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
-module github.com/anderson/authserver
+module github.com/Anderson1218/ec-auth-server
 
 go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.3
+	github.com/lib/pq v1.3.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/subosito/gotenv v1.2.0
+	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915
+)
--- main.go
+++ main.go
@@ -1,19 +1,31 @@
-// main.go
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"github.com/Anderson1218/ec-auth-server/controllers"
+	"github.com/Anderson1218/ec-auth-server/driver"
+	"github.com/Anderson1218/ec-auth-server/utils"
+	"github.com/gorilla/mux"
+	"github.com/subosito/gotenv"
 	"log"
 	"net/http"
 )
 
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
 func main() {
+	db = driver.ConnectDB()
+	router := mux.NewRouter()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from Docker123")
-	})
+	controller := controllers.Controller{}
 
-	fmt.Println("Listening on :8080")
+	router.HandleFunc("/api/users/", controller.Signup(db)).Methods("POST")
+	router.HandleFunc("/api/users/token", controller.Login(db)).Methods("POST")
+	router.HandleFunc("/api/users/me", utils.TokenVerifyMiddleWare(controller.Me(db))).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
